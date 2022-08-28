17d126280a39dfc920a22e5da56bbaadedc5965e
jbaptista2 <jbaptista@gobots.com.br>
2022-08-28 00:01:49 -0300

    criando migrations

---
+++ data/migrations/20220828025050_create-products.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("products", (tbl) => {
+    tbl.increments("id").primary();
+    tbl.text("name", 128).notNullable();
+    tbl.integer("quantity").notNullable();
+    tbl.integer("category_id", 256).references('category.id');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("products");
+};
+
