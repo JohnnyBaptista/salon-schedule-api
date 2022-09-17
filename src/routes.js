215b17d581e0790bf1ae2b63077bcca57cc17ee8
Felipe Freitas <felipe.sfreitas14@gmail.com>
2022-09-17 14:26:34 -0300

    feito as modificacoes pedidas, qualquer duvida favor entrar em contato

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const users = require('./routes/users');
-
+const brands = require('./routes/brands');
 const routes = express.Router();
 
 routes.use('/users', users);
