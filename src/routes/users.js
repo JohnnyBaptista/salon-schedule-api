e8248deb3e4f283697871242646110edcf834f4f
jbaptista2 <jbaptista@gobots.com.br>
2022-09-19 23:43:25 -0300

    fix: removendo rotas de brand do users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const userController = require('../controller/UsersController');
-const brandController = require('../controller/BrandController');
 
 const routes = express.Router();
 
@@ -9,11 +8,6 @@ routes.post('/', userController.createUser);
 routes.get('/:id', userController.getUserById);
 routes.patch('/:id', userController.updateUser);
 routes.delete('/:id', userController.deleteUser);
-// Endpoints for brands
-routes.get('/', brandController.getAllBrand);
-routes.post('/', brandController.createBrand);
-routes.get('/:id', brandController.getBrandById);
-routes.patch('/:id', brandController.updateBrand);
-routes.delete('/:id', brandController.deleteBrand);
+
 
 module.exports = routes;
