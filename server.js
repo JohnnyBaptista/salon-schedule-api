cfb9f52383318c0ca5fd38d33b96de13dd4f6281
jbaptista2 <jbaptista@gobots.com.br>
2022-11-29 09:39:05 -0300

    fix: server

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const routes = require('./src/routes');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const routes = require("./src/routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(routes);
-
 const PORT = process.env.PORT || 3333;
 
+const whitelist = ["*"];
+
+app.use((req, res, next) => {
+  const origin = req.get("referer");
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  }
+  if (req.method === "OPTIONS") res.sendStatus(200);
+  else next();
+});
+
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
+
+app.use(routes);
+
+
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
+  console.log(`Server running at port ${PORT}`);
 });
