519a50dfc2242652597348ae349326a9b87e95db
jbaptista2 <jbaptista@gobots.com.br>
2022-11-29 23:20:47 -0300

    ref removendo inutil

--- server.js
+++ server.js
@@ -8,37 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const whitelist = ["*"];
-
-app.use((req, res, next) => {
-  const origin = req.get("referer");
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,Content-Type,Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  }
-  if (req.method === "OPTIONS") res.sendStatus(200);
-  else next();
-});
-
-const setContext = (req, res, next) => {
-  if (!req.context) req.context = {};
-  next();
-};
-
-app.use(setContext);
-
 app.use(routes);
 
-module.exports = app;
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
