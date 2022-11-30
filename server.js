62b224e49dafe323e9388daa6a162958d8642136
jbaptista2 <jbaptista@gobots.com.br>
2022-11-30 00:40:31 -0300

    frontend

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 require("dotenv").config();
 const routes = require("./src/routes");
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 app.use(express.json());
 
-app.use(routes);
+app.use("/api", routes);
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 3333;
 
